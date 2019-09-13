fc605b5ffe34894e75943f3de7ce5accc09fada4
marquesr1 <rafael.fmarques@outlook.com>
2019-09-13 10:12:22 -0300

    Navigation

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import Routes from './routes';
+
+const App = () => {
+    return <Routes />;
+};
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import { createStackNavigator } from 'react-navigation';
+
+import Login from './pages/login';
+
+export default createStackNavigator({
+    Login
+});

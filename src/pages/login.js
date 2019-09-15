784cb8497d23a9f03b45424f06d91eb4ba5f077c
marquesr1 <rafael.fmarques@outlook.com>
2019-09-14 22:07:31 -0300

    Login page and NativeBase dependencies

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {
+    Container,
+    Label,
+    Content,
+    Form,
+    Item,
+    Input,
+    Button,
+    Text
+} from 'native-base';
 
 export default class login extends Component {
     static navigationOptions = {
@@ -7,9 +17,37 @@ export default class login extends Component {
     };
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
+            <Container style={styles.Container}>
+                <Content padder>
+                    <Form>
+                        <Item floatingLabel>
+                            <Label style={styles.Text}>E-mail</Label>
+                            <Input style={styles.Text} />
+                        </Item>
+                        <Item floatingLabel>
+                            <Label style={styles.Text}>Senha</Label>
+                            <Input style={styles.Text} secureTextEntry />
+                        </Item>
+                    </Form>
+                    <Button primary style={styles.Button}>
+                        <Text>Entrar</Text>
+                    </Button>
+                </Content>
+            </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    Container: {
+        backgroundColor: '#051e3e'
+    },
+    Button: {
+        justifyContent: 'center',
+        backgroundColor: '#451e3e',
+        marginTop: 40
+    },
+    Text: {
+        color: '#FFF'
+    }
+});
